4af7559b4a906ade87c7cb5773f792b17b66cc2b
Mark Peters <mapeters101@earthlink.net>
2025-05-29 17:39:59 -0400

    Work on lesson056.

--- lesson056.py
+++ lesson056.py
@@ -1,7 +1,7 @@
 name = input("Please enter your name: ")
 age = int(input("Please enter your age: "))
 
-if 18 < age < 31:
+if 18 <= age < 31:
     print("{}, welcome to the holiday.".format(name))
 else:
     print("Sorry, {}, you are not between the ages of 18 and 31.".format(name))
